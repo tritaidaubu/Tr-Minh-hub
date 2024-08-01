a48443d52f6eb202206dd9891e3d0f090c849938
tritaidaubu <166419203+tritaidaubu@users.noreply.github.com>
2024-08-01 07:30:51 +0700

    Add files via upload

--- AnhTaibeta.lua
+++ AnhTaibeta.lua
@@ -68,8 +68,10 @@ if success then
 else
     print("Failed to send profile information to Discord: " .. response)
 end
-local SolarisLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/kickTh/New-Ui/main/SolarisLib.lua.txt"))()
-
+----------------------------------------------------------------------------------------------------------------------------------------------
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 ----------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
     Title = "Trí Minh Hub | Blox Fruits",
